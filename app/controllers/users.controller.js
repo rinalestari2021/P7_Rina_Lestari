2439daf563ec51b5bf56dd4ce3818cf269b260e6
Rina Lestari <javagraphiste@outklook.comgit inittouch index.htmlgit add.git commit -mgit commit -m first>
2022-02-21 22:16:55 +0100

    controller 210222

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -13,7 +13,7 @@ exports.signup = (req, res, next) => {
       });
       user
         .save()
-        .then(() => res.status(201).json({ message: "User created !" }))
+        .then(() => res.status(201).json({ message: "User profile created !" }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
@@ -24,7 +24,7 @@ exports.login = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        return res.status(401).json({ error: "User not found !" });
+        return res.status(401).json({ error: "User profile not found !" });
       }
     })
     .catch((error) => res.status(500).json({ error }));
